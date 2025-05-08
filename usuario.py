d84016fc48471c9080d00a2a5fa9c2067bc0fa9e
Santiago-Bracamonte <santiagobracamonte01@gmail.com>
2025-05-08 03:19:35 -0300

    Primer commit clase usuario y atributos

---
+++ usuario.py
@@ -0,0 +1,8 @@
+class Usuario:
+    def __init__(self, id_nombre, nombre, apellido, contrasena, email, rol):
+        self.id_nombre = id_nombre
+        self.nombre = nombre
+        self.apellido = apellido
+        self.contrasena = contrasena
+        self.email = email
+        self.rol = rol
